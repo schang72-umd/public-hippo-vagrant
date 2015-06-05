87df3b1b5a442cd262d8ec40a17e72f3d4f8d0f8
Peter Eichman <peichman@umd.edu>
2015-06-05 16:45:09 -0400

    LIBWEB-2456. Bootstrap provisioning (JDK & Tomcat)
    Added bootstrap provisioning shell script to install the JDK and Tomcat.
    Preferred versions to match production are:
    
    - JDK: 7u71
    - Tomcat: 7.0.57
    
    JDK must be present in dist, but the shell script will download Tomcat if it
    doesn't find it.
    
    https://issues.umd.edu/browse/LIBWEB-2456

---
+++ dist/README.md
@@ -0,0 +1,7 @@
+# public-hippo-vagrant/dist
+
+This is the directory that holds the tarballs for some of the applications
+needed to run HippoCMS. At a minimum, you must download an Oracle JDK into this
+directory (see [the main README](../README.md) for more information).
+
+All \*.tar.gz files in this directory are [ignored by git](../.gitignore).
---
+++ scripts/bootstrap.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mkdir -p /apps
+
+# Oracle JDK
+JDK=$(find /vagrant/dist -maxdepth 1 -type f -name 'jdk-*.tar.gz' | tail -n1)
+if [ -z "$JDK" ]; then
+    echo >&2 "No JDK found. Please download an Oracle JDK tar.gz and place it in [vagrant]/dist"
+    exit 1
+fi
+echo "Found JDK in $JDK"
+tar xzvf "$JDK" --directory /apps
+# find the newly extracted JDK
+JAVA_HOME=$(find /apps -maxdepth 1 -type d -name 'jdk*' | tail -n1)
+ln -s "$JAVA_HOME" /apps/java
+cat > /etc/profile.d/hippo.sh <<END
+export JAVA_HOME=$JAVA_HOME
+export PATH=\$JAVA_HOME/bin:\$PATH
+END
+
+# Tomcat
+# can't install via yum since that is only version 6.0.24
+TOMCAT_VERSION=7.0.57
+TOMCAT=/vagrant/dist/apache-tomcat-${TOMCAT_VERSION}.tar.gz
+# look for a cached tarball
+if [ ! -e "$TOMCAT" ]; then
+    TOMCAT_PKG_URL=http://archive.apache.org/dist/tomcat/tomcat-7/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz
+    wget -O "$TOMCAT" "$TOMCAT_PKG_URL"
+fi
+tar xvzf "$TOMCAT" --directory /apps
+chown -R vagrant:vagrant /apps/apache-tomcat-${TOMCAT_VERSION}
