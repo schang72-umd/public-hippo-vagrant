c5a654df3bb3267c19c098de2fb846c22a39806e
Paul Hammer <phammer@umd.edu>
2016-07-14 14:35:53 -0400

    LIBWEB-3268. Modify tomcat.sh to include creation and provisioning of site2
    https://issues.umd.edu/browse/LIBWEB-3268

--- scripts/tomcat.sh
+++ scripts/tomcat.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-mkdir -p /apps/cms/storage-{cms,site}/workspaces
+mkdir -p /apps/cms/storage-{cms,site,site2}/workspaces
 cp -rpv /vagrant/env/tomcat /apps/cms/tomcat-cms
 cp -rpv /vagrant/env/tomcat /apps/cms/tomcat-site
-cp -rpv /vagrant/env/tomcat-{cms,site} /apps/cms
+cp -rpv /vagrant/env/tomcat /apps/cms/tomcat-site2
+cp -rpv /vagrant/env/tomcat-{cms,site,site2} /apps/cms
 # directories needed by Tomcat
-mkdir -p /apps/cms/tomcat-{cms,site}/{logs,temp}
+mkdir -p /apps/cms/tomcat-{cms,site,site2}/{logs,temp}
